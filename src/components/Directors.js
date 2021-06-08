33628521f72b7c91d7d2beeda0d65713474105e4
osxgirl <osxgirl@mac.com>
2021-06-08 12:43:06 -0400

    commit tree 7a6addaef11b75a80dde119b620ee4ebd9f0c970

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,16 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director=> (
+        <div>
+          {director.name}
+          <br/>
+          <ul>
+            {director.movies.map(movie=> <li>{movie}</li>)}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
