fef763dc78a215eb7cb9e422ebaa0dd2392a1dc9
osxgirl <osxgirl@mac.com>
2021-06-08 12:43:11 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie)=> (
+          <div>
+            {movie.title}
+            <br/>
+            {movie.time}
+            <br/>
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
